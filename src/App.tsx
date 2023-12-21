f6ae05d49c81a3f6fb2d842c3be9882e82cfeb6a
Sushant <sushantlamsal31@gmail.com>
2023-12-21 18:51:41 +0545

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+//this is a primary router component
+import "./globals.css"
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+//NOTE:this is the entry point of the application
+import ReactDom from "react-dom/client";
+import App from "./App";
+
+ReactDom.createRoot(document.getElementById("root")!).render(
+  <App />
+)
