738b73c285e451c3e52046744c55e0822e1cc313
Sushant <sushantlamsal31@gmail.com>
2023-12-27 16:34:16 +0545

    feat:mobile bottombar

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,3 +1,4 @@
+import Bottombar from '@/components/shared/Bottombar'
 import LeftSidebar from '@/components/shared/LeftSidebar'
 import Topbar from '@/components/shared/Topbar'
 import React from 'react'
@@ -7,10 +8,11 @@ const RootLayout = () => {
   return (
     <div className='w-full md:flex'>
       <Topbar/>
-      <LeftSidebar/>
+      <LeftSidebar/>    
       <section className='flex flex-1 h-full'>
         <Outlet />
       </section>
+      <Bottombar/>
     </div>
   )
 }
