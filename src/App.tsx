2f7fde01c66877a21fa7c17098bfa9fd8e3ce990
Sushant <sushantlamsal31@gmail.com>
2023-12-27 14:01:11 +0545

    initial:pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@
 import {Routes , Route} from 'react-router-dom';
 import "./globals.css"
 import SigninForm from './_auth/forms/SigninForm';
-import { Home } from './_root/pages';
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile, } from './_root/pages';
 import SignupForm from './_auth/forms/SignupForm';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
@@ -30,6 +30,14 @@ const App = () => {
 
         {/* Index Below means that it is the starting page of the application and element to render a component in the browser  */}
         <Route index element={<Home/>} />
+        <Route path='/explore' element={<Explore/>}/>
+        <Route path='/saved' element={<Saved/>}/>
+        <Route path='/all-users' element={<AllUsers/>}/>
+        <Route path='/create-post' element={<CreatePost/>}/>
+        <Route path='/update-post/:id' element={<EditPost/>}/>
+        <Route path='/posts/:id' element={<PostDetails/>}/>
+        <Route path='/profile/:id/*' element={<Profile/>}/>
+        <Route path='/update-profile/:id' element={<UpdateProfile/>}/>
         </Route>
       </Routes>
       <Toaster />
