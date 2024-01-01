7687f0613eb1b3630963045874c0ff210bfd621a
Sushant <sushantlamsal31@gmail.com>
2024-01-01 18:57:41 +0545

    test

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -7,7 +7,7 @@ import {
   useQueryClient,
   useInfiniteQuery
 } from '@tanstack/react-query'
-import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from '../appwrite/api'
+import { createPost, createUserAccount, getRecentPosts, likePost, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewPost, INewUser } from '@/types'
 import { QUERY_KEYS } from './queryKeys'
 
@@ -19,6 +19,7 @@ export const useCreateUserAccount = () => {
     }
   )
 }
+
 export const useSignInAccount = () => {
   return useMutation(
     {
@@ -29,6 +30,7 @@ export const useSignInAccount = () => {
     }
   )
 }
+
 export const useSignOutAccount = () => {
   return useMutation(
     {
@@ -36,6 +38,7 @@ export const useSignOutAccount = () => {
     }
   )
 }
+
 export const useCreatePost = () => {
   const queryClient = useQueryClient();
   return useMutation(
@@ -48,9 +51,37 @@ export const useCreatePost = () => {
       }
     })
 }
+
 export const useGetRecentPosts = () => {
   return useQuery({
   queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
   queryFn: getRecentPosts,
 })
 }
+
+export const useLikePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({
+      postId,
+      likesArray,
+    }: {
+      postId: string;
+      likesArray: string[];
+    }) => likePost(postId, likesArray),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+      });
+    },
+  });
+};
