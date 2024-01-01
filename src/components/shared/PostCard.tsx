9076c424c28582e357219b1f26ad08ba91ba7b52
Sushant <sushantlamsal31@gmail.com>
2024-01-01 18:39:33 +0545

    feat:like and save post

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -2,6 +2,7 @@ import { useUserContext } from "@/context/AuthContext";
 import { formatDateString } from "@/lib/utils";
 import { Models } from "appwrite"
 import { Link } from "react-router-dom";
+import PostStats from "./PostStats";
 
 type PostCardProps = {
   post: Models.Document;
@@ -53,6 +54,7 @@ const PostCard = ({post}: PostCardProps) => {
         </div>
         <img src={post.imageUrl || 'assets/icons/profile-placeholder.svg'} className="post-card_img" alt="Post Img" />
       </Link>
+      <PostStats post={post} userId={user.id}/>
     </div>
   )
 }
