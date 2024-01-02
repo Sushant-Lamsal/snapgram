87eacb9b583e6a2be35da7363015149e519df2a4
Sushant <sushantlamsal31@gmail.com>
2024-01-02 07:23:18 +0545

    Feat: Save Post

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -2,7 +2,8 @@ import { useUserContext } from "@/context/AuthContext";
 import { useDeleteSavedPost, useGetCurrentUser, useLikePost, useSavePost } from "@/lib/react-query/queriesAndMutations";
 import { checkIsLiked } from "@/lib/utils";
 import { Models } from "appwrite";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Loader from "./Loader";
 
 type PostStatsProps = {
   post: Models.Document;
@@ -16,11 +17,16 @@ const PostStats = ({post , userId} : PostStatsProps) => {
   const [isSaved, setIsSaved] = useState(false)
 
   const {mutate:likePost} = useLikePost()
-  const {mutate:savePost} = useSavePost()
-  const {mutate:deleteSavedPost} = useDeleteSavedPost()
+  const {mutate:savePost , isPending: isSavingPost} = useSavePost()
+  const {mutate:deleteSavedPost , isPending: isDeletingSaved} = useDeleteSavedPost()
 
   const {data: currentUser} = useGetCurrentUser();
 
+  const savedPostRecord = currentUser?.save.find((record: Models.Document)=> record.post.$id === post.$id)
+  useEffect(() => {
+    setIsSaved(!!savedPostRecord)
+  }, [currentUser])
+  
   const handleLikePost = (e: React.MouseEvent) => {
     e.stopPropagation();
 
@@ -40,7 +46,7 @@ const PostStats = ({post , userId} : PostStatsProps) => {
 
   const handleSavePost = (e: React.MouseEvent) => {
     e.stopPropagation();
-    const savedPostRecord = currentUser?.save.find((record: Models.Document)=> record.$id === post.$id)
+    
 
     if(savedPostRecord){
       setIsSaved(false)
@@ -66,14 +72,15 @@ const PostStats = ({post , userId} : PostStatsProps) => {
         </p>
       </div>
       <div className="flex gap-2">
-        <img 
+      {isSavingPost || isDeletingSaved ? <Loader/> :
+      <img 
         src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
         alt="save" 
         width={20} 
         height={20}
         onClick={handleSavePost}
         className="cursor-pointer"
-        />
+        />}
       </div>
     </div>
   )
